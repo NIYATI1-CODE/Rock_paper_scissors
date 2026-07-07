d647db25036cc131b8a1e309a3a4c7829a389810
NIYATI1-CODE <niyatisingh629@gmail.com>
2026-07-07 10:01:35 +0000

    Create function playGame This function compares choices conditionally & then give output accordingly

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function getHumanChoice() {
     alert("Try again , I guess!");
   }
 }
+function playRound(Human, Comp) {
+  Human = Human.toLowerCase();
+  if (Human == "rock" && Comp == "paper") {
+    console.log("You LOSE! Paper beats Rock");
+    computerScore++;
+  }
+  else if (Human == "paper" && Comp == "rock") {
+    console.log("You WIN! Paper beats Rock")
+    humanScore++;
+  }
+  else if (Human == "scissors" && Comp == "paper") {
+    console.log("You WIN! Scissors beats Paper");
+    humanScore++;
+  }
+  else if (Human == "rock" && Comp == "scissors") {
+    console.log("You WIN! Rock beats Scissors");
+    humanScore++;
+  }
+  else if (Human == "paper" && Comp == "scissors") {
+    console.log("You LOSE! Scissors beats Paper");
+    computerScore++;
+  }
+  else if (Human === Comp) {
+    console.log("TIE...It seems like we both won.");
+  }
+  else {
+    console.log("You LOSE! Scissors beats Rock");
+    computerScore++;
+  }
+}
